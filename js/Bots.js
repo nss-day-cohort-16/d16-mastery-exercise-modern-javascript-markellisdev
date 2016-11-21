39c0c66b45f27425ecf211b620f297037adc3b54
Mark Ellis <markellisdev@gmail.com>
2016-11-20 22:11:55 -0600

    began bot js information

---
+++ js/Bots.js
@@ -0,0 +1,68 @@
+"use strict";
+/* jshint esversion: 6 */
+
+let Robots = {};
+
+	Battledome.Robot = {};
+
+	/* Base ROBOT ------------- */
+	let Model = () =>
+		this.name = "plain robot"
+		this.weapon = "shockwave";
+		this.health = "200";
+
+
+	/* Robot TYPES ----------- */
+	Battledome.Robot.Drone
+		let this.minHealth = 50;
+		let this.maxHealth = 100;
+		this.health = Math.floor(Math.random() * (this.maxHealth - this.minHealth +1)) + this.minHealth;
+		this.armor
+		this.property2
+
+	let Luna_X_2000 = () =>
+		this.speed = 70;
+		this.img = `http://www.panzerbaer.de/helper/pix/bw_drohne_luna_katapult-001.jpg`
+
+	let MQ9_Reaper = () =>
+		this.cost = "$16.9 million";
+		this.img = `<img id="lcms_fancybox-image" class="lcms_fancybox-image lcms_fancybox-img" src="/Websites/gaasi/PhotoGallery/4264089/mq9-reaper-weaponized.jpg?02045" alt="USAF MQ-9 Reaper">`
+
+
+	Battledome.Robot.Bipedal
+		let this.minHealth = 50;
+		let this.maxHealth = 140;
+		this.health = Math.floor(Math.random() * (this.maxHealth - this.minHealth +1)) + this.minHealth;
+		this.armor
+		this.property2
+
+	let Megabot = () =>
+		this.skill = ;
+		this.
+		this.img = `http://images6.fanpop.com/image/photos/38400000/Megabot-big-hero-6-38439105-500-281.jpg`
+
+	let R2D2 = () =>
+		this.
+		this.
+		this.img = `https://elrincondelmae.files.wordpress.com/2015/12/star_3.jpg`
+
+
+
+
+	Battledome.Robot.ATV
+		let this.minHealth = 60;
+		let this.maxHealth = 120;
+		this.health = Math.floor(Math.random() * (this.maxHealth - this.minHealth +1)) + this.minHealth;
+		this.armor //function
+		this.property2
+
+	let Roomba980 = () =>
+		this.skill = "super clean floors"
+		this.
+		this.img = `http://i.imgur.com/hsUooRJ.jpg`
+
+	let GrizzlyRUV = () =>
+		this.towCapacity = "1400lbf";
+		this.bullbar = () =>
+			this.health + 15;
+		this.img = `https://www.clearpathrobotics.com/wp-content/uploads/2015/09/Grizzly-RUV_mining-in-sand.jpg`
--- js/battledome.js
+++ js/battledome.js
@@ -3,36 +3,11 @@
 
 let Battledome = Battledome =>
 
-	Battledome.Robot = {};
-
-	/* Base ROBOT ------------- */
-	Battledome.Robot.Player = name =>
-		this.type = null;
-
-		this.playerName = null;
-		this.health = Math.floor(Math.random() * (max - min +1)) + min;
-		this.
-
-	/* Robot TYPES ----------- */
-	Battledome.Robot.Drone
-		let this.minHealth = 50;
-		let this.maxHealth = 100;
-		this.property1
-		this.property2
-		this.health = Math.floor(Math.random() * (max - min +1)) + min;
+	Battledome.Player = function(name) {
+		name
+		skill
+		health
+		damage
+	};
 
 
-	Battledome.Robot.Bipedal
-		let this.minHealth = 50;
-		let this.maxHealth = 140;
-		this.property1
-		this.property2
-		this.health = Math.floor(Math.random() * (max - min +1)) + min;
-
-
-	Battledome.Robot.ATV
-		let this.minHealth = 60;
-		let this.maxHealth = 120;
-		this.armor //function
-		this.property2
-		this.health = Math.floor(Math.random() * (max - min +1)) + min;
