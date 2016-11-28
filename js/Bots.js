7f13412c27a42f59cd98362df48295adaf77ac18
Mark Ellis <markellisdev@gmail.com>
2016-11-28 10:16:14 -0600

    getting closer with prototype

--- js/Bots.js
+++ js/Bots.js
@@ -1,98 +1,114 @@
 "use strict";
 /* jshint esversion: 6 */
 
-var Robot = require('./Battledome.js');
+// var Robot = require('./Battledome.js');
 
-let Robots = {};
+// Defining the Base object for any player in the game\
+
+let Battledome = {}
+
+	Battledome.Player = function(name) {
+		// this.toString = function() {
+			this.name = "Whizzle snips";
+			this.skill = null;
+			this.health = null;
+			this.damage = null;
+		// };
+};
+
+	Battledome.Player.prototype.setName = (name) => {
+		this.name = name;
+	};
+
 
 	/* Base ROBOT ------------- */
-	let Model = function Model() {
+	Battledome.Player.Model = function Model() {
 		this.weapon = "shockwave";
 		this.health = 0;
 		this.damage = 0;
 	};
-	Model.prototype = new Player();
+	Battledome.Player.Model.prototype = new Battledome.Player();
 
 
 	/* Robot TYPES ----------- */
 
 		/* Drones --------- */
 
-	let Drone = function Drone() {
+	Battledome.Player.Drone = function Drone() {
 		this.minHealth = 50;
 		this.maxHealth = 100;
 		this.health = Math.floor(Math.random() * (this.maxHealth - this.minHealth +1)) + this.minHealth;
-		this.armor
-		this.property2
+		this.skill = "flying";
 	};
-	Drone.prototype = new Model();
+	Battledome.Player.Drone.prototype = new Battledome.Player.Model();
 
-	var drone = new Drone();
-	console.log("Drone", drone);
+	// var drone = new Battledome.Combatants.Drone();
+	// console.log("Drone", drone);
 
 
-	Robots.Luna_X_2000 = function Luna_X_2000() {
+	Battledome.Player.Luna_X_2000 = function Luna_X_2000() {
 		this.speed = 70;
 		this.img = `http://www.panzerbaer.de/helper/pix/bw_drohne_luna_katapult-001.jpg`
 	};
-	Robots.Luna_X_2000.protype = new Drone();
+	Battledome.Player.Luna_X_2000.protype = new Battledome.Player.Drone();
 
-	Robots.MQ9_Reaper = function MQ9_Reaper() {
-		this.cost = "$16.9 million";
-		this.img = `<img id="lcms_fancybox-image" class="lcms_fancybox-image lcms_fancybox-img" src="/Websites/gaasi/PhotoGallery/4264089/mq9-reaper-weaponized.jpg?02045" alt="USAF MQ-9 Reaper">`
-	};
-	Robots.MQ9_Reaper.protoype = new Drone();
+	// Robots.MQ9_Reaper = function MQ9_Reaper() {
+	// 	this.cost = "$16.9 million";
+	// 	this.img = `<img id="lcms_fancybox-image" class="lcms_fancybox-image lcms_fancybox-img" src="/Websites/gaasi/PhotoGallery/4264089/mq9-reaper-weaponized.jpg?02045" alt="USAF MQ-9 Reaper">`
+	// };
+	// Robots.MQ9_Reaper.protoype = new Drone();
 
-		/* Bipedals --------- */
+	// 	/* Bipedals --------- */
 
-	let Bipedal = function Bipedal() {
-		this.minHealth = 50;
-		this.maxHealth = 140;
-		this.health = Math.floor(Math.random() * (this.maxHealth - this.minHealth +1)) + this.minHealth;
-		this.armor
-		this.property2
-	};
-	Bipedal.prototype = new Model();
+	// let Bipedal = function Bipedal() {
+	// 	this.minHealth = 50;
+	// 	this.maxHealth = 140;
+	// 	this.health = Math.floor(Math.random() * (this.maxHealth - this.minHealth +1)) + this.minHealth;
+	// 	this.armor
+	// 	this.property2
+	// };
+	// Bipedal.prototype = new Model();
 
-	Robots.Megabot = function Megabot() {
-		this.skill =
-		this.
-		this.img = `http://images6.fanpop.com/image/photos/38400000/Megabot-big-hero-6-38439105-500-281.jpg`
-	};
-	Robots.Megabot.prototype = new Bipedal();
+	// Robots.Megabot = function Megabot() {
+	// 	this.skill =
+	// 	this.
+	// 	this.img = `http://images6.fanpop.com/image/photos/38400000/Megabot-big-hero-6-38439105-500-281.jpg`
+	// };
+	// Robots.Megabot.prototype = new Bipedal();
 
-	Robots.R2D2 = function R2D2() {
-		this.skill = "multiple languages"
-		this.
-		this.img = `https://elrincondelmae.files.wordpress.com/2015/12/star_3.jpg`
-	};
-	Robots.R2D2.prototype = new Bipedal();
+	// Robots.R2D2 = function R2D2() {
+	// 	this.skill = "multiple languages"
+	// 	this.
+	// 	this.img = `https://elrincondelmae.files.wordpress.com/2015/12/star_3.jpg`
+	// };
+	// Robots.R2D2.prototype = new Bipedal();
 
-		/* ATVs --------- */
+	// 	/* ATVs --------- */
 
-	let ATV = function ATV() {
-		this.minHealth = 60;
-		this.maxHealth = 120;
-		this.health = Math.floor(Math.random() * (this.maxHealth - this.minHealth +1)) + this.minHealth;
-		this.armor //function
-		this.property2
-	};
-	ATV.prototype = new Model();
+	// let ATV = function ATV() {
+	// 	this.minHealth = 60;
+	// 	this.maxHealth = 120;
+	// 	this.health = Math.floor(Math.random() * (this.maxHealth - this.minHealth +1)) + this.minHealth;
+	// 	this.armor //function
+	// 	this.property2
+	// };
+	// ATV.prototype = new Model();
 
-	Robots.Roomba980 = function Roomba980() {
-		this.skill = "super clean floors"
-		this.
-		this.img = `http://i.imgur.com/hsUooRJ.jpg`;
-	};
-	Robots.Roomba980.prototype = new ATV();
+	// Robots.Roomba980 = function Roomba980() {
+	// 	this.skill = "super clean floors"
+	// 	this.
+	// 	this.img = `http://i.imgur.com/hsUooRJ.jpg`;
+	// };
+	// Robots.Roomba980.prototype = new ATV();
 
 
-	Robots.GrizzlyRUV = function GrizzlyRUV() {
-		this.towCapacity = "1400lbf";
-		this.bullbar = (function () {
-			this.health + 15;
-		});
-		this.img = `https://www.clearpathrobotics.com/wp-content/uploads/2015/09/Grizzly-RUV_mining-in-sand.jpg`;
-	};
-	Robots.GrizzlyRUV.prototype = new ATV();
+	// Robots.GrizzlyRUV = function GrizzlyRUV() {
+	// 	this.towCapacity = "1400lbf";
+	// 	this.bullbar = (function () {
+	// 		this.health + 15;
+	// 	});
+	// 	this.img = `https://www.clearpathrobotics.com/wp-content/uploads/2015/09/Grizzly-RUV_mining-in-sand.jpg`;
+	// };
+	// Robots.GrizzlyRUV.prototype = new ATV();
 
+module.exports =  Battledome;
--- js/battledome.js
+++ js/battledome.js
@@ -1,24 +1,14 @@
 "use strict";
 /* jshint esversion: 6 */
 
-let Battledome = function() {
-
-
-	this.Player = function(name) {
-		this.name = name;
-		this.skill = null;
-		this.health = null;
-		this.damage = null;
-	};
-};
+var Robot = require('./Bots.js');
+var $ = require('jquery');
 
 // let testPlayer = new Player("Jim");
 
 // console.log("testPlayer", testPlayer);
-
-function Player (name){
-	this.name = name;
-}
+	var drone = new Robot.Player.Drone("Frank");
+	console.log("Drone", drone);
 
 var debug=false;
 
@@ -27,18 +17,32 @@ function comment(msg) {
 		console.log("message", msg);
 	}
 }
-
+/*-- Function to begin attack --*/
+	//This captures values for player names and robot type
 $("#beginAttack").click( function(e) {
 	comment("Begin attack click");
 	debugger
 	let name1 = $("#player1-name").val();
 	let name2 = $("#player2-name").val();
-	let p1 = new Player(name1);
-	let p2 = new Player(name2);
+	let p1model = $("#player1-model").val();
+	let p1 = new Robot.Player();
+	let p2 = new Robot.Player();
+	p1.name = name1;
+
+	console.log("player1", p1);
+	console.log("player 1 model", p1model);
+
+
 	let nextCard = $(this).attr("next");
 	let moveAlong = false;
 });
 
+$(".selectpicker").each(function() {
+	let model1Selected = $("#player1-model option:selected").value;
+	let model2Selected = $("#player2-model option:selected").value;
+	// let p1 = new Robot.Robots.`$(model1Selected)`;
+	// let p2 = new Robot.Robots.`$(model1Selected)`;
+});
 
-	// Battledome.Player.name = $("#player1-name");
-module.exports =  {Player};
+
+
